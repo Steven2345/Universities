4db356a2c20281b618c12da6138943019d1db433
Cseke <a@b.com>
2024-05-15 20:14:31 +0300

    all restricted middleware calls now include HttpContext

--- backend/Program.cs
+++ backend/Program.cs
@@ -4,6 +4,7 @@ using backend.Service;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 
 //Generator.populateUniversities();
@@ -43,24 +44,37 @@ app.UseCors(MyAllowSpecificOrigins);
 
 app.MapGet("/", () => { Console.WriteLine("hit"); return universityService.GetBatchExtended(0, 10); });
 app.MapGet("/{page}", (int page) => universityService.GetBatchExtended(page, 10));
-app.MapGet("/names", () => universityService.GetAllNames());
+app.MapGet("/names", () => universityService.GetAllNames())
+        .RequireAuthorization();
 app.MapGet("/details/{id}", (int id) => universityService.getById(id));
-app.MapPost("/add", (UniversityNoId uni) =>
-    universityService.AddUniversity(new University(1, uni.Name, uni.Location, uni.Score, uni.Description)));
-app.MapPut("/edit/{id}", (UniversityNoId uni, int id) =>
-    universityService.UpdateUniversity(new University(id, uni.Name, uni.Location, uni.Score, uni.Description)));
-app.MapDelete("/delete/{id}", (int id) =>
-    universityService.DeleteUniversity(id));
+app.MapPost("/add", (UniversityNoId uni, HttpContext context) =>
+    universityService.AddUniversity(new University(1, uni.Name, uni.Location, uni.Score, uni.Description,
+                                            context.User.FindFirstValue(ClaimTypes.NameIdentifier))))
+        .RequireAuthorization();
+app.MapPut("/edit/{id}", (UniversityNoId uni, int id, HttpContext context) =>
+    universityService.UpdateUniversity(new University(id, uni.Name, uni.Location, uni.Score, uni.Description,
+                                               context.User.FindFirstValue(ClaimTypes.NameIdentifier))))
+        .RequireAuthorization();
+app.MapDelete("/delete/{id}", (int id, HttpContext context) =>
+    universityService.DeleteUniversity(id))
+        .RequireAuthorization();
 
 app.MapGet("/faculties", () => facultyService.GetBatch(0, 50));
 app.MapGet("/faculties/{page}", (int page) => facultyService.GetBatch(page, 50));
 app.MapGet("/faculties/details/{id}", (int id) => facultyService.getById(id));
-app.MapPost("/faculties/add", (FacultyNoId fcl) =>
-    facultyService.AddFaculty(new Faculty(1, fcl.Name, fcl.NoOfStudents, fcl.UniversityID)));
-app.MapPut("/faculties/edit/{id}", (FacultyNoId fcl, int id) =>
-    Console.WriteLine(fcl));
-app.MapDelete("/faculties/delete/{id}", (int id) =>
-    facultyService.DeleteFaculty(id));
+app.MapPost("/faculties/add", (FacultyNoId fcl, HttpContext context) =>
+    facultyService.AddFaculty(new Faculty(1, fcl.Name, fcl.NoOfStudents, fcl.UniversityID,
+                                      context.User.FindFirstValue(ClaimTypes.NameIdentifier))))
+        .RequireAuthorization();
+app.MapPut("/faculties/edit/{id}", (FacultyNoId fcl, int id, HttpContext context) =>
+    facultyService.UpdateFaculty(new Faculty(id, fcl.Name, fcl.NoOfStudents, fcl.UniversityID,
+                                         context.User.FindFirstValue(ClaimTypes.NameIdentifier))))
+        .RequireAuthorization();
+app.MapDelete("/faculties/delete/{id}", (int id, HttpContext context) =>
+    facultyService.DeleteFaculty(id))
+        .RequireAuthorization();
+
+app.MapIdentityApi<IdentityUser>();
 
 app.Run();
 
