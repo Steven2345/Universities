28987769efe59d42ff06e42879620a69d13dcd02
Cseke <a@b.com>
2024-05-17 03:42:06 +0300

    database deployed and app tables reference the generated user table

--- backend/script.sql
+++ backend/script.sql
@@ -6,7 +6,7 @@ create table Universities (
 	uni_location varchar(50),
 	uni_score float, 
 	uni_descr varchar(500),
-	[user_id] varchar(40) not null,
+	[user_id] nvarchar(450),
 	constraint PK_uni_id primary key (uni_id)
 )
 
@@ -15,7 +15,14 @@ create table Faculties (
 	facult_name varchar(50),
 	facult_nostud int,
 	uni_id int not null,
-	[user_id] varchar(40) not null,
+	[user_id] nvarchar(450),
 	constraint PK_facult_id primary key (facult_id),
 	constraint FK_uni_id foreign key (uni_id) references Universities(uni_id) on delete cascade
 )
+
+
+alter table [dbo].[Universities]
+add constraint FK_user_id foreign key ([user_id]) references [dbo].[AspNetUsers](Id) on delete set null
+
+alter table [dbo].[Faculties]
+add constraint FK_user_id foreign key ([user_id]) references [dbo].[AspNetUsers](Id) on delete set null
